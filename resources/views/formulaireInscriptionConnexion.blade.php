b844e007d1cb4e0c4373c02dcf3a41de04ecc7e3
Carmen T <carmentodorutz@yahoo.com>
2024-04-29 19:25:25 +0200

    Ajoute de formulaireInscriptionConnexion

---
+++ resources/views/formulaireInscriptionConnexion.blade.php
@@ -0,0 +1,15 @@
+<html>
+<h1>Connectez vous ou creez un compte utilisateur</h1>
+<form method="post" action="">
+    <label for="name">Nom</label>
+    <input type="text" id="name" name="name" required>
+    <label for="firstname">Prenom</label>
+    <input type="text" id="firstname" name="firstname">
+    <label for="email">Email - example : nom@example.com</label>
+    <input type="text" id="email" name="email" required>
+    <label for="password">Mot de passe</label>
+    <input type="password" id="password" name="password" required minlenght="8">
+    <input type="submit" value="S'inscrire">
+
+</form>
+</html>
