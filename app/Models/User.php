f41677d002e2fda294b8a272370f31e9061538ef
Ardui Thomas <ardui.thomas@gmail.com>
2024-05-03 20:31:11 +0200

    modeleutilisateurmodif

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,21 +1,17 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+protected $table="_inscription";
     protected $fillable = [
         'name',
         'firstname',
