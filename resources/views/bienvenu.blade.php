b57393e2cd7edace4c4647d1ddcffdb66ddf14e1
Anthony Velez <josuevelez@outlook.es>
2024-05-16 10:41:40 +0200

    ajout modif fichier bienvenu

--- resources/views/bienvenu.blade.php
+++ resources/views/bienvenu.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Bienvenue </title>
+
+</head>
+<body>
+<h1 id="content">Bienvenue sur Bike My Ability</h1>
+<p>Grace a bike my ability trouver votre vélos adapté. <br>
+
+
+</body>
+</html>
