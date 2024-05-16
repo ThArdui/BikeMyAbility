6888ffcfc3bca728902be7686d7b6ad119ca8561
unknown <christianhonore@outlook.com>
2024-05-16 14:52:45 +0200

    results

---
+++ resources/views/expanded-bike.blade.php
@@ -0,0 +1,8 @@
+<div class="bike-card expanded">
+    <div class="bike-preview">
+        <img src="{{ $bike->Picture }}" alt="{{ $bike->Bike_name }}">
+    </div>
+    <h2>{{ $bike->Bike_name }}</h2>
+    <p>{{ $bike->Description }}</p>
+    <!-- Ajoutez d'autres attributs que vous souhaitez afficher dans la carte -->
+</div>
