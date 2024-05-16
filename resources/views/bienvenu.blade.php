8ce5de3de79edc24cdf7f132810789ecaaed605e
unknown <christianhonore@outlook.com>
2024-05-16 19:39:23 +0200

    bienvenu

--- resources/views/bienvenu.blade.php
+++ resources/views/bienvenu.blade.php
@@ -1,4 +1,5 @@
 @extends('template.projet')
+
 @section('titre')
 <title>Bienvenue </title>
 @endsection
@@ -8,7 +9,12 @@
 
 @section('content')
 <h1 id="bienvenue">Bienvenue sur Bike My Ability</h1>
-<p>Grace a bike my ability trouver votre vélos adapté. <br>
+<p>L' equipe des 'Bikers' ont le plaisir de vous presenter une solution pour aider
+    toutes les personnes qui soit atteint d un handicap a trouve un velo adapte a
+    leur type d'handicap . <br></p>
+<img src="{{ asset('pictures/fun2gobike.jpg') }}" alt="velo">
+<h2> createur du site </h2>
+    <p>Thomas chef de groupe et createur de l idee de projet , et ses coequipier Carmen , Anthony et Christian .</p>
 
 @endsection
 @section('footer')
