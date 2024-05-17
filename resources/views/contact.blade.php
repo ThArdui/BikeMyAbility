022bbba668911d6415764824b45c1e24f439918a
Ardui Thomas <ardui.thomas@gmail.com>
2024-05-17 16:21:59 +0200

    mentions champs formulaires de contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,24 +4,26 @@ Nous contacter!
 @endsection
 @section('content')
     <h1>Comment nous contacter?</h1>
-    <p>Si vous voullez nous proposer d'autres velos adaptés ou si vous êtes un revendeur des velos et vous avez envie d'ajouter des velos dans notre base des donnés, envoyez nous un message en utilisant ce formulaire. Un de nos bikers va vous repondre dans le plus bref delai.</p>
+
+    <p>Si vous voulez nous proposer d'autres velos adaptés ou si vous êtes un revendeur de vélos et vous avez envie d'en ajouter dans notre base des données, envoyez nous un message en utilisant ce formulaire. Un de nos bikers vous répondra dans les plus brefs délais.</p>
+    <p> * Champs obligatoires</p>
     <form method="post" action="{{ route('contact') }}" class="col-md-6 mx-auto">
         @csrf
         <div class="form-group">
-            <label for="nom">Votre nom et prenom :</label>
+            <label for="nom">Votre nom et prenom <span aria-hidden="true"> * </span> :</label>
             <input type="text" class="form-control" id="nom" name="nom" max="255" required>
         </div>
         <div class="form-group">
-            <label for="email">Email - exemple : nom@example.com</label>
+            <label for="email">Email ( ex : nom@example.com) <span aria-hidden="true"> * </span> :</label>
             <input type="email" class="form-control" id="email" name="email" max="255" required>
         </div>
         <div class="form-group">
-            <label for="phone">Votre numero gsm :</label>
+            <label for="phone">Votre numéro de gsm <span aria-hidden="true"> * </span> :</label>
             <input type="tel" class="form-control" id="phone" name="phone" max="20">
         </div>
         <div class="form-group">
-            <label for="message">Votre message :</label>
-            <input type="text" class="form-control" id="message" name="message" max="2500" min="20" required>
+            <label for="message">Votre message <span aria-hidden="true"> * </span> :</label>
+            <textarea class="form-control" id="message" name="message"  cols="10" rows="8" required>  </textarea>
         </div>
         <button type="submit" class="btn btn-primary">Envoyez le message</button>
     </form>
