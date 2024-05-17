902b155549228ecdba0422a198d5017a98dfa7a1
Carmen T <carmentodorutz@yahoo.com>
2024-05-17 11:15:03 +0200

    MOdification la vue bienvenu

--- resources/views/bienvenu.blade.php
+++ resources/views/bienvenu.blade.php
@@ -14,10 +14,11 @@
             <p>L' equipe des 'Bikers' ont le plaisir de vous presenter une solution pour aider
                 toutes les personnes  atteintes d' un handicap à trouver un vélo adapté à
                 leur situation. </p>
-            <img src="{{ asset('pictures/fun2gobike.jpg') }}" alt="velo">
-            <h2> L'équpe  des Bikers </h2>
+            <img src="{{ asset('pictures/fun2gobike.jpg') }}" alt="velo decrit pour le lecteur d'ecran">
+            <h2> L'équipe  des Bikers </h2>
             <p> Carmen, Christian, Anthony et Thomas espère que vous aurez une agréable expérience sur BikeMyAbility.</p>
-
+<p>Si vous voullez nous contacter, on est la pour vous :</p>
+            <li><a href="{{ route('contact') }}" aria-current="{{ request()->is('contact') ? 'page' : '' }}">Nous contacter</a></li>
 
         </div>
     </div>
