efb08773b65444eb05ecbc5c14dc0587e9e97026
unknown <christianhonore@outlook.com>
2024-05-18 11:41:36 +0200

    result

--- resources/views/results-bikes.blade.php
+++ resources/views/results-bikes.blade.php
@@ -3,33 +3,24 @@
     Resultat de votre recherche
 @endsection
 @section('content')
+<!-- resources/views/results-bike.blade.php -->
+<h1>Résultats de la recherche</h1>
+<ul>
+    @foreach ($results as $bike)
+        <li>{{ $bike->Bike_name }}</li>
+        <!-- Affichez d'autres attributs du vélo -->
+    @endforeach
+</ul>
+@foreach($results as $bike)
+    <div class="cadre-velo">
+        <img src="{{$bike->Picture}}" alt="{{$bike->Bike_name}}">
+        <p>{{$bike->Description}}</p>
+        {{--
+         <a href="{{route('bike.detail',$bikes->id)}}">
+             <i class="fas fa-arrow-right"></i>
+         </a>
+         --}}
+    </div>
 
-    resources/views/results-bike.blade.php -->
-    @if (count($results) > 0)
-        Résultats de la recherche
-
-        @foreach ($results as $bike)
-            {{ $bike->Bike_name }}
-
-            Affichez d'autres attributs du vélo -->
-        @endforeach
-
-        @foreach($results as $bike)
-
-            class="cadre-velo">
-
-            src="{{$bike->Picture}}" alt="{{$bike->Bike_name}}">
-            {{$bike->Description}}
-            {{--
-
-href="{{route('bike.detail',$bikes->id)}}">
-
-class="fas fa-arrow-right">
-
---}}
-
-        @endforeach
-    @else
-        Aucun vélo disponible.
-    @endif
+@endforeach
 @endsection
