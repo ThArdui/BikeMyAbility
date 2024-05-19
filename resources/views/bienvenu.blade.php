a3fc76c1e86b04d6617785e4cb4e0480b03d1828
Carmen T <carmentodorutz@yahoo.com>
2024-05-19 23:04:04 +0200

    Petite correction pour la page bienvenue

--- resources/views/bienvenu.blade.php
+++ resources/views/bienvenu.blade.php
@@ -1,14 +1,4 @@
-@extends('template.projet')
-
-@section('titre')
-<title>Bienvenue </title>
-@endsection
-@section('nav')
-    @include('navBar')
-@endsection
-
-@section('content')
-    <div class="row">
+<div class="row">
         <!-- changements thomas -->
         <div class="col-12 text-center">
             <h1 id="bienvenue" class="text-danger-emphasis"> Bienvenue sur Bike My Ability</h1>
@@ -23,7 +13,7 @@
     </div>
 
     <h2 class="text-danger-emphasis"> A propos</h2>
-    <p> Bike My Ability est né du constat que beaucoup de  personnes en situation de handicap peuvent se demander comment elles peuvent profiter du plaisir cyclable malgré leur condition. Certeines, ignorent même que  cela est possible.  Le but de  Bike My Ability est de montrer qu'il existe une solution cyclable adapté à tout un chacun.</p>
+    <p> Bike My Ability est né du constat que beaucoup de  personnes en situation de handicap peuvent se demander comment elles peuvent profiter du plaisir cyclable malgré leur condition. Certains, ignorent même que  cela est possible.  Le but de  Bike My Ability est de montrer qu'il existe une solution cyclable adapté à tout un chacun.</p>
 
     <h2 class="text-danger-emphasis"> L'équipe  des Bikers </h2>
     <p> L'équipe  des Bikers, c'est nous.... Carmen, Christian, Anthony et Thomas.</p>
@@ -31,8 +21,3 @@
     <p> N'hésitez  pas à nous contacter, nous vous aiderons avec plaisir:</p>
 
     <p>   <a href="{{ route('contact') }}" aria-current="{{ request()->is('contact') ? 'page' : '' }}">Nous contacter</a> </p>
-
-@endsection
-@section('footer')
-    @include('footer')
-@endsection
