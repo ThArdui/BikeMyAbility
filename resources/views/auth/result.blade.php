00ec12f9360a0158d599153d89ae51d7f93f2dbc
Carmen T <carmentodorutz@yahoo.com>
2024-05-22 09:54:38 +0200

    Modif page reussie

--- resources/views/auth/result.blade.php
+++ resources/views/auth/result.blade.php
@@ -1,2 +1,9 @@
-{{ $success }}
+@extends('template.projet')
+@section('titre')
+    Inscription reusie!
+@endsection
+@section('content')
+<h1 class="text-center">{{ $success }}</h1>
+<p>Vous êtes connecté et vous pouvez rechercher votre vélo adapté ci-dessous!</p>
 <a href="{{ route('bikesearch') }}">Trouver un vélo.</a>
+@endsection
