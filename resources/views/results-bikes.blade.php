744c362635be3726d3d73c74d397b39506370d34
Carmen T <carmentodorutz@yahoo.com>
2024-05-22 10:27:23 +0200

    Ajoute de lien sur le photos de resultat de recherche et lien pour repeter la recherche

--- resources/views/results-bikes.blade.php
+++ resources/views/results-bikes.blade.php
@@ -14,7 +14,8 @@
         </ul>
         @foreach($results as $bike)
             <div class="cadre-velo">
-                <img src="{{$bike->Picture}}" alt="{{$bike->Bike_name}}">
+                <a href="{{ route('details.velo', $bike->id) }}">
+                    <img src="{{$bike->Picture}}" alt="{{$bike->Bike_name}}"></a>
                 <p>{{$bike->Description}}</p>
                 {{--
                  <a href="{{route('bike.detail',$bikes->id)}}">
@@ -25,6 +26,9 @@
         @endforeach
     @else
         <h1>Résultats de la recherche</h1>
-        <p>Aucun vélo disponible</p>
+        <p>Aucun vélo disponible. Veillez repeter la recherche avec d'autres critères.</p>
+        <a href="{{route('bikesearch')}}">Rechercher un vélo adapté</a>
+        <p>Ou regardez la gallerie photo avec tous les velos disponibles :</p>
+        <a href="{{route('gallerie.velos')}}">Galerie photos </a>
     @endif
 @endsection
