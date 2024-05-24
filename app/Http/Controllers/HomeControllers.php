ea16272f036364d3f18df87691c35ee02d63e8ee
Johnny Piette <johnny.piette@gmail.com>
2024-05-24 14:53:32 +0200

    Updates

--- app/Http/Controllers/HomeControllers.php
+++ app/Http/Controllers/HomeControllers.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-class HomeControllers extends controller
+class HomeControllers extends Controller
 {
     public function index()
     {
